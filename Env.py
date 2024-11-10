ede4f424d36052448a930423e6f93e3f349e64c2
JvG <Jurrianvangeresteijn@gmail.com>
2024-11-10 13:55:09 +0100

    Moved user configurable things from .env to config.yaml for a more user friendly approach.

--- Env.py
+++ Env.py
@@ -1,15 +1,24 @@
-import os
 import json
-from dotenv import load_dotenv
+import yaml
 
-load_dotenv()
+def load_config(attribute, config_file='config.json'):
+    try:
+        with open(config_file, 'r') as file:
+            config = json.load(file)
+        value = config.get(attribute)
+        if value is None:
+            raise ValueError(f"Missing required configuration: {attribute}")
+        return value
+    except FileNotFoundError:
+        raise FileNotFoundError(f"The configuration file '{config_file}' was not found. Please create config.json following the format of the template.")
+    except json.JSONDecodeError:
+        raise ValueError(f"Error decoding the JSON in the configuration file '{config_file}'. Please create config.json following the format of the template.")
+    except Exception as e:
+        print(f"Error loading {config_file}: {e}")
+        raise
 
-def load_config(attribute):
-    """Attribute as Cookies or Headers"""
-    with open('config.json', 'r') as file:
-        config = json.load(file)
-        
-    return config.get(attribute)
+with open("config.yaml", "r") as file:
+    yaml_conf = yaml.safe_load(file)
 
 class Env:
     _instance = None
@@ -23,17 +32,22 @@ class Env:
     def __init__(self):
         if Env._instance is not None:
             raise Exception("This is a singleton!")
-        self.cook = load_config('Cookies')
-        self.head = load_config('Headers')
-        self.meta = os.getenv('METADATA')
-        self.simi = os.getenv('SIMBAN')
-        self.disc = os.getenv('BANDIS')
-        self.band = os.getenv('BANDPAR')
-        self.deta = os.getenv('DETAIL')
-        self.memb = os.getenv('MEMBER')
-        self.url_modi= os.getenv('URLMODIFIED')
-        self.url_band= os.getenv('URLBANDS')
-        self.retries = os.getenv('RETRIES')
-        self.delay = os.getenv('DELAY')
+        
+        try:
+            self.cook = load_config('Cookies')
+            self.head = load_config('Headers')
+        except ValueError as e:
+            print(f"Error: {e}")
+            raise
+        self.meta = yaml_conf['paths']['METADATA']
+        self.simi = yaml_conf['paths']['MA_SIMILAR']
+        self.disc = yaml_conf['paths']['MA_DISCOG']
+        self.band = yaml_conf['paths']['MA_BANDS']
+        self.deta = yaml_conf['paths']['MA_DETAILS']
+        self.memb = yaml_conf['paths']['MA_MEMBER']
+        self.url_modi= yaml_conf['urls']['MODIFIED']
+        self.url_band= yaml_conf['urls']['BANDS']
+        self.retries = yaml_conf['scraper']['RETRIES']
+        self.delay = yaml_conf['scraper']['DELAY']
 
 env = Env.get_instance()
