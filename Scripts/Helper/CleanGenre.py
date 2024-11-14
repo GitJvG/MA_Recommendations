c29ccfe54401ae8015f6bc62c7cedc3be7b2a6fe
JvG <Jurrianvangeresteijn@gmail.com>
2024-11-14 15:19:17 +0100

    Removed unused library

--- Scripts/Helper/CleanGenre.py
+++ Scripts/Helper/CleanGenre.py
@@ -1,11 +1,9 @@
 import sys
 import os
-
 project_root = os.path.abspath(os.path.join(os.path.dirname(__file__), '../..'))
 sys.path.append(project_root)
-
-import pandas as pd
 import re
+
 from Env import Env
 env= Env.get_instance()
 
