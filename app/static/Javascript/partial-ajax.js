00f977d0aff9857aa540c957a7541089f10834bb
JvG <Jurrianvangeresteijn@gmail.com>
2024-11-19 20:19:01 +0100

    Partial-ajax now also works on images with the ajax-link class

--- app/static/Javascript/partial-ajax.js
+++ app/static/Javascript/partial-ajax.js
@@ -1,7 +1,17 @@
 import { fetchContent } from './utils.js'
 
 document.addEventListener('click', function (e) {
+    let url = null;
+
     if (e.target.classList.contains('ajax-link')) {
         e.preventDefault();
-        var url = e.target.getAttribute('href');
-        fetchContent(url)}})
+        url = e.target.getAttribute('href');
+    } else if (e.target.tagName === 'IMG' && e.target.parentElement.classList.contains('ajax-link')) {
+        e.preventDefault();
+        url = e.target.parentElement.getAttribute('href');
+    }
+
+    if (url) {
+        fetchContent(url);
+    }
+});
