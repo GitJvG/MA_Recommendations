647152c31322254495f1184d599c7f5f4c4764e5
Jurrian van Geresteijn <Jurrianvangeresteijn@gmail.com>
2024-12-11 19:26:05 +0100

    Made the embedded video player resize-able

--- app/static/Javascript/embed.js
+++ app/static/Javascript/embed.js
@@ -1,4 +1,5 @@
 function fetchVideo(bandName, albumName, AlbumType) {
+  AlbumType = (AlbumType === 'Full Length') ? 'Full Album' : AlbumType;
   const searchQuery = `${bandName} ${albumName} ${AlbumType}`;
   const url = `/ajax/youtube_search?q=${encodeURIComponent(searchQuery)}`;
 
@@ -8,25 +9,29 @@ function fetchVideo(bandName, albumName, AlbumType) {
       if (data && data.video_url) {
         const videoEmbedUrl = data.video_url;
 
+        const videoWrapper = document.createElement('div');
+        videoWrapper.classList.add('floating-video-wrapper');
+
         const iframe = document.createElement('iframe');
         iframe.src = videoEmbedUrl;
         iframe.width = '560';
         iframe.height = '315';
         iframe.allow = 'accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture';
         iframe.allowFullscreen = true;
-        iframe.classList.add('floating-video');
 
         const closeBtn = document.createElement('button');
         closeBtn.classList.add('close-btn');
         closeBtn.innerHTML = '&times;';
 
         closeBtn.addEventListener('click', function () {
-          iframe.remove();
-          closeBtn.remove();
+          videoWrapper.remove();
         });
 
-        document.body.appendChild(iframe);
-        iframe.appendChild(closeBtn);
+        videoWrapper.appendChild(iframe);
+        videoWrapper.appendChild(closeBtn);
+
+        document.body.appendChild(videoWrapper);
+        makeResizable(videoWrapper);
       } else {
         alert('No video found for this album and band.');
       }
@@ -36,3 +41,37 @@ function fetchVideo(bandName, albumName, AlbumType) {
     });
 }
 
+function makeResizable(wrapper) {
+  const resizer = document.createElement('div');
+  resizer.classList.add('resizer');
+  wrapper.appendChild(resizer);
+
+  let isResizing = false;
+
+  resizer.addEventListener('mousedown', (e) => {
+    isResizing = true;
+    document.body.style.cursor = 'se-resize';
+
+    initialWidth = wrapper.offsetWidth;
+    initialHeight = wrapper.offsetHeight;
+    initialMouseX = e.clientX;
+    initialMouseY = e.clientY;
+  });
+
+  let initialWidth, initialHeight, initialMouseX, initialMouseY;
+
+  document.addEventListener('mousemove', (e) => {
+    if (isResizing) {
+      const deltaX = initialMouseX - e.clientX;
+      const deltaY = initialMouseY - e.clientY;
+
+      wrapper.style.width = `${initialWidth + deltaX}px`;
+      wrapper.style.height = `${initialHeight + deltaY}px`;
+    }
+  });
+
+  document.addEventListener('mouseup', () => {
+    isResizing = false;
+    document.body.style.cursor = 'auto';
+  });
+}
