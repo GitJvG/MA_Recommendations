9dc28e48c50e3bdc6b64c7b138de16eb8777c652
Jurrian van Geresteijn <Jurrianvangeresteijn@gmail.com>
2024-12-12 20:05:45 +0100

    Added index=false to discography scraper

--- Scripts/Components/AlbumScraper.py
+++ Scripts/Components/AlbumScraper.py
@@ -44,7 +44,7 @@ def refresh():
     # Because discog currently doesn't have a reliable unique id combination (that could be de-duplicated afterwards), all to be processed ids are first deleted.
     df = pd.read_csv(env.disc)
     df = df[~df["band_id"].isin(band_ids_to_process)]
-    df.to_csv(env.disc)
+    df.to_csv(env.disc, index=False)
 
     Parallel_processing(band_ids_to_process, 200, env.disc, fetch_album_data)
     
