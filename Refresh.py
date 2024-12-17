e331c9771c81871fd75d864b605e00026f005778
Jurrian van Geresteijn <Jurrianvangeresteijn@gmail.com>
2024-12-17 21:59:57 +0100

    Updated module references in the all encompassing wrapper

--- Refresh.py
+++ Refresh.py
@@ -1,6 +1,6 @@
 from Scripts.Refresh import refresh as Refresh_Source_Datasets
-from DataScripts.M_Datasets import main as Refresh_Processed_Datasets
-from DataScripts.SQL import refresh_tables as Push_To_Sql
+from Scripts.M_Datasets import main as Refresh_Processed_Datasets
+from Scripts.SQL import refresh_tables as Push_To_Sql
 
 def main():
     Refresh_Source_Datasets()
