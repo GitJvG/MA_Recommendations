7c0420681a6b0b9492c04bcd928ba64f45bc1a1a
Jurrian van Geresteijn <Jurrianvangeresteijn@gmail.com>
2025-04-04 21:40:46 +0200

    Added viewport to base.html to stop content from scrolling underneath the header. Also updated NLTK check related init.py

--- src/MA_Scraper/Scripts/Add_Proc/Helper/__init__.py
+++ src/MA_Scraper/Scripts/Add_Proc/Helper/__init__.py
@@ -1,7 +1,2 @@
-import sys
-import os
-project_root = os.path.abspath(os.path.join(os.path.dirname(__file__), '..', '..'))
-sys.path.append(project_root)
-
-from Scripts.Add_Proc.Helper.SetupNLTK import setup
+from MA_Scraper.Scripts.Add_Proc.Helper.SetupNLTK import setup
 setup()
