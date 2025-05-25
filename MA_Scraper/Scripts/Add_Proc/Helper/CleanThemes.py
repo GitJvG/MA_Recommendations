ed364d0eb69bd089e127a5fd6ca5d9d3677e3424
Jurrian van Geresteijn <Jurrianvangeresteijn@gmail.com>
2025-05-25 21:31:05 +0200

    More aggressive theme normalisation with per word lemmatizing and stemming

--- MA_Scraper/Scripts/Add_Proc/Helper/CleanThemes.py
+++ MA_Scraper/Scripts/Add_Proc/Helper/CleanThemes.py
@@ -1,10 +1,12 @@
 import unicodedata
-from nltk.stem import WordNetLemmatizer
+from nltk.stem import WordNetLemmatizer, PorterStemmer, LancasterStemmer
 import re
+import pandas as pd
 
 from MA_Scraper.Env import Env
 env = Env.get_instance()
 lemmatizer = WordNetLemmatizer()
+stemmer = PorterStemmer()
 
 def normalize_to_ascii(theme):
     normalized = unicodedata.normalize('NFD', theme)
@@ -12,18 +14,15 @@ def normalize_to_ascii(theme):
         
     return ascii_text
 
-def lemmatize_string(theme):
-    return ' '.join([lemmatizer.lemmatize(word) for word in theme.split()])
-
-
-def basic_processing(themes):
-    theme = themes.lower()
+def basic_processing(theme):
+    theme = theme.lower()
     theme = re.sub(r'\(.*?\)', '', theme) # removes anything between parenthesis
     theme = re.sub(r'\s?/\s', '/', theme) # Removes spaces before and after '/'
 
     theme = normalize_to_ascii(theme)
     theme = re.sub(r'[^\x20-\x7E]', '', theme) # Removes non-ASCII
     theme = re.sub(r'\b(of|the|a|an|to)\b', '', theme) # remove common words
+    #theme = re.sub(r'-', ' ', theme)
     theme = re.sub(r'\s+', ' ', theme) # Reduce consecutive spaces to one space
 
     theme = re.sub(r';', ',', theme) # Replace semicolon with a comma. Metallum uses this for time related distinctions but that isn't an important distinction for me.
@@ -31,6 +30,17 @@ def basic_processing(themes):
     theme = re.sub(r'[()]+', '', theme).strip() # Removes remaining parenthesis
     theme = re.sub(r'\band\b', ',', theme) # Replace 'and' with a comma
     theme = re.sub(r'\s*,\s*', ',', theme)
-    theme = lemmatize_string(theme)
+    themes = []
+    for theme in theme.split(','):
+        if not theme:
+            continue
+        longest_word = max(theme.split(), key=len)
+        if not longest_word:
+            continue
 
+        lemmatized_word = lemmatizer.lemmatize(longest_word)
+        stemmed_word = stemmer.stem(lemmatized_word)
+        themes.append(stemmed_word)
+
+    theme = ','.join(themes) if themes else None
     return theme.strip() if theme else None
