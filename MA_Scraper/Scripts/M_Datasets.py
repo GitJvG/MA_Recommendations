6f2734ae62db37dd70c4f96a514f1f713d852a7a
Jurrian van Geresteijn <Jurrianvangeresteijn@gmail.com>
2025-06-24 20:22:14 +0200

    Removed candidate generation from centralized processed dataset flow

--- MA_Scraper/Scripts/M_Datasets.py
+++ MA_Scraper/Scripts/M_Datasets.py
@@ -2,7 +2,6 @@
 from MA_Scraper.Scripts.Add_Proc.ThemeDict import update_pickle as Theme_Anchors
 from MA_Scraper.Scripts.Add_Proc.DIM_Theme import main as theme_main
 from MA_Scraper.Scripts.Add_Proc.DIM_GenrePrefix import main as genre_main
-from MA_Scraper.Scripts.Add_Proc.candidates import main as canidates
 from MA_Scraper.Scripts.Add_Proc.CombinedBand import main as bands
 
 def main():
@@ -10,7 +9,6 @@ def main():
     Theme_Anchors() # Scans Details dataset for new themes, performs analysis and creates theme anchors with fuzzy matching.
     theme_main() # Generates theme.csv based on the updated pickle and indexes themes for each band into themes.csv
     genre_main() # Generates genre.csv and genres.csv
-    canidates() # Generate user-specific candidates
 
 if __name__ == '__main__':
     main()
